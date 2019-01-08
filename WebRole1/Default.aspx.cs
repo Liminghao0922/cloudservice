f7adff9ac94e0108684621aea0416d5a73099c91
Minghao Li <minghl@microsoft.com>
2019-01-08 16:52:28 +0800

    first init

---
+++ WebRole1/Default.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web.UI;
+
+namespace WebRole1
+{
+    public partial class _Default : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //Value of Textbox1 and TectBox2 is assigin on the ViewState
+            ViewState["name"] = TextBox1.Text;
+            ViewState["password"] = TextBox2.Text;
+            //after clicking on Button TextBox value Will be Cleared
+            TextBox1.Text = TextBox2.Text = string.Empty;
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            //If ViewState Value is not Null then Value of View State is Assign to TextBox
+            if (ViewState["name"] != null)
+            {
+                TextBox1.Text = ViewState["name"].ToString();
+            }
+            if (ViewState["password"] != null)
+            {
+                TextBox2.Text = ViewState["password"].ToString();
+            }
+        }
+    }
+}
---
+++ WebRole1/Global.asax.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Web;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace WebRole1
+{
+    public class Global : HttpApplication
+    {
+        private void Application_Start(object sender, EventArgs e)
+        {
+            // Code that runs on application startup
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            //this.PreSendRequestHeaders += Global_PreSendRequestHeaders;
+            //EndRequest += Global_EndRequest;
+        }
+        protected void Application_PreSendRequestHeaders(object sender, EventArgs e)
+        {
+            string instanceId = Environment.MachineName;
+            if (Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance != null)
+            {
+                instanceId += "-" + Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance.Id;
+            }
+            HttpApplication app = sender as HttpApplication;
+            app?.Context?.Response.Headers.Add("x-cloudservice-instance-v2", instanceId);
+        }
+        //private void Global_EndRequest(object sender, EventArgs e)
+        //{
+        //    string instanceId = Environment.MachineName;
+        //    if (Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance != null)
+        //    {
+        //        instanceId += "-" + Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance.Id;
+        //    }
+
+        //    Response.Headers.Add("x-cloudservice-instance", instanceId);
+        //}
+
+        //private void Global_PreSendRequestHeaders(object sender, EventArgs e)
+        //{
+        //    string instanceId = Environment.MachineName;
+        //    if (Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance != null)
+        //    {
+        //        instanceId += "-" + Microsoft.WindowsAzure.ServiceRuntime.RoleEnvironment.CurrentRoleInstance.Id;
+        //    }
+
+        //    Response.Headers.Add("x-cloudservice-instance", instanceId);
+        //}
+    }
+}
